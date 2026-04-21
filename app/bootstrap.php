8e85138e833dde940ec289dfd9d7627ce2b47fba
Julyellen Santos <julyellens.w5i@hotmail.com>
2026-04-21 04:02:13 -0300

    feat: Set default timezone in bootstrap file
    - Added logic to set the default timezone based on the application configuration.
    - Fallback to 'America/Sao_Paulo' if the timezone is not specified in the config file.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -13,3 +13,9 @@ if (!is_file($autoload))
 require $autoload;
 
 \App\Core\Env::load(dirname(__DIR__) . '/config/.env');
+
+$config = require dirname(__DIR__) . '/config/app.php';
+$tz = isset($config['timezone']) ? (string) $config['timezone'] : 'America/Sao_Paulo';
+if (!@date_default_timezone_set($tz)) {
+    date_default_timezone_set('America/Sao_Paulo');
+}
