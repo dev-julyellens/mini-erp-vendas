0fd0c767769873f32eaa917ec26c16c045368b4f
Julyellen Santos <julyellens.w5i@hotmail.com>
2026-05-24 23:47:26 -0300

    feat: adiciona configurações de segurança para sessões

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,20 @@
 
 declare(strict_types=1);
 
+ini_set('session.cookie_httponly', '1');
+ini_set('session.cookie_samesite', 'Strict');
+ini_set('session.use_strict_mode', '1');
+ini_set('session.use_only_cookies', '1');
+
+if (
+    (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== '' && $_SERVER['HTTPS'] !== 'off')
+    || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+)
+{
+    ini_set('session.cookie_secure', '1');
+}
+
+session_name('mini_erp_session');
 session_start();
 
 $autoload = dirname(__DIR__) . '/vendor/autoload.php';
@@ -16,6 +30,7 @@ require $autoload;
 
 $config = require dirname(__DIR__) . '/config/app.php';
 $tz = isset($config['timezone']) ? (string) $config['timezone'] : 'America/Sao_Paulo';
-if (!@date_default_timezone_set($tz)) {
+if (!@date_default_timezone_set($tz))
+{
     date_default_timezone_set('America/Sao_Paulo');
 }
