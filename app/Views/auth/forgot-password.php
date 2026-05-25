0ff188f09bf5b80964c37e528c971ff3a386d469
Julyellen Santos <julyellens.w5i@hotmail.com>
2026-05-25 01:43:06 -0300

    docs: adiciona anotação de tipo para variável $url nas views de autenticação

--- app/Views/auth/forgot-password.php
+++ app/Views/auth/forgot-password.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+/** @var callable(string):string $url */
 /** @var array<string, string> $errors */
 /** @var array<string, mixed> $old */
 /** @var ?string $resetUrl */
--- app/Views/auth/reset-password.php
+++ app/Views/auth/reset-password.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+/** @var callable(string):string $url */
 /** @var string $token */
 /** @var array<string, string> $errors */
 
