c763cce609b4237d9b2b51fb4c47c7a510728472
Julyellen Santos <julyellens.w5i@hotmail.com>
2026-05-25 21:38:13 -0300

    feat: habilita emulação de prepared statements no PDO

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -29,6 +29,7 @@ final class Database
                 self::$instance = new PDO($dsn, $db['username'], $db['password'], [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false,
                 ]);
             }
             catch (PDOException $e)
